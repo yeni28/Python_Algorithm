831128e76ab9081606b24e9a857a24fe3e6b3b62
광주2반_용승민 <yongsm295@gmail.com>
2022-08-11 17:58:21 +0900

    0811 note

--- 0811/4843_특별한정렬.py
+++ 0811/4843_특별한정렬.py
@@ -27,3 +27,26 @@ for tc in range(1,T+1):
 
     print(f'#{tc} {sort_num}')
 
+
+    #other sol #깃랩 올라온 것 참고해서 정리하기
+
+    T =
+
+    index = 0 #바꿀 원소의 인덱스
+    i = 0 # 탐색을 시작할 위치
+
+    for ni in range(10):
+        #정렬시작
+        for j in range(i, n): #최대값 또는 최솟값을 찾기 시작
+            if ni % 2 == 0 and numbers[j] > numbers[index]:
+                #최대값의 인덱스
+                index = j
+            if ni % 2 == 1 and numbers[j] > numbers[index]:
+                index = j
+                #최소값의 인덱스
+
+            i += 1
+
+            for i in range(10):
+                print()
+
--- 220811_baekjun_15072.py
+++ 220811_baekjun_15072.py
@@ -1,2 +1,8 @@
 N, M = map(int, input().split())
 reward = list(map(int, input().split()))
+
+for i in range(M):
+    numbers = list(map(int, input().split()))
+
+    score = 0
+    if
