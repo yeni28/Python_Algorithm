58612676895ed1208829db5f6488bb7861dea413
광주2반_용승민 <yongsm295@gmail.com>
2022-08-11 17:45:39 +0900

    other sol add

--- 0811/4839_이진탐색.py
+++ 0811/4839_이진탐색.py
@@ -39,3 +39,42 @@ for tc in range(1, T+1):
         result = 'A'
 
     print(f'#{tc} {result}')
+
+
+    #other sol
+
+    winner = "" #승리자
+    a_start, a_end = 1, p #a의 이진탐색 범위
+    b_start, b_end = 1, p #b의 이진탐색 범위
+
+    while True:
+        a_find = False #a가 페이지를 찾았는지
+        b_find = False #b가 페이지를 찾았는지
+
+        mid = (a_start + a_end) // 2
+
+        if mid == a:
+            a_find =True
+        elif mid > a:
+            a_end = mid
+        else:
+            a_start = mid
+        #b의 페이지를 찾기 시작
+        mid = (b_start + b_end) // 2
+
+        if mid == b:
+            b_find = True
+        elif mid > b:
+            b_end = mid
+        else:
+            b_start = mid
+
+        if a_find and b_find: #순서를 바꾸면동시에 찾았을 떄도 A가 나오니까 맨 위로
+            answer = 0
+            break
+        if a_find:
+            answer = 'A'
+            break
+        if b_find:
+            answer = 'B'
+            break
